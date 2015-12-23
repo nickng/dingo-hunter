5be6c89168c2a54af1f41265dc7881fdd0f77af7
Nicholas Ng <nickng@projectfate.org>
2015-12-23 12:05:41 +0000

    CFSM: ending GotoNodes should not override
     - Use role instead of string name to key maps

--- sesstype/generator/cfsm.go
+++ sesstype/generator/cfsm.go
@@ -8,19 +8,19 @@ import (
 )
 
 var (
-	stateCount     = make(map[string]int)     // Contains next state number for each CFSM.
-	labelJumpState = make(map[string]string)  // Convert label names to state names to jump to.
-	finalState     = make(map[string]string)  // Final states for each role.
-	cfsmByName     = make(map[string]int)     // Converts role names to CFSM state number.
-	chanByIndex    = make([]sesstype.Chan, 0) // Index -> Chan
+	stateCount     = make(map[sesstype.Role]int)    // Contains next state number for each CFSM.
+	labelJumpState = make(map[string]string)        // Convert label names to state names to jump to.
+	finalState     = make(map[sesstype.Role]string) // Final states for each role.
+	cfsmByName     = make(map[sesstype.Role]int)    // Converts role names to CFSM state number.
+	chanByIndex    = make([]sesstype.Chan, 0)       // Index -> Chan
 	cfsmCount      = 0
 	chanCount      = 0
 )
 
-func genNewState(roleName string) string {
-	stateIdx := stateCount[roleName]
-	stateCount[roleName]++
-	return fmt.Sprintf("q%d%d", cfsmByName[roleName], stateIdx)
+func genNewState(role sesstype.Role) string {
+	stateIdx := stateCount[role]
+	stateCount[role]++
+	return fmt.Sprintf("q%d%d", cfsmByName[role], stateIdx)
 }
 
 func isAlphanum(r rune) bool {
@@ -55,12 +55,12 @@ func encodeSymbols(name string) string {
 }
 
 // Create CFSM for channel.
-func genChanCFSM(name string, typ string, begin int, end int) string {
-	q0 := fmt.Sprintf("%s", encodeSymbols(genNewState(name)))
-	qTerm := fmt.Sprintf("%s", encodeSymbols(genNewState(name)))
+func genChanCFSM(role sesstype.Role, typ string, begin int, end int) string {
+	q0 := fmt.Sprintf("%s", encodeSymbols(genNewState(role)))
+	qTerm := fmt.Sprintf("%s", encodeSymbols(genNewState(role)))
 	cfsm := ""
 	for i := begin; i < end; i++ {
-		q1 := encodeSymbols(genNewState(name))
+		q1 := encodeSymbols(genNewState(role))
 		cfsm += fmt.Sprintf("%s %d ? %s %s\n", q0, i, encodeSymbols(typ), q1)
 		for j := begin; j < end; j++ {
 			if i != j {
@@ -70,25 +70,26 @@ func genChanCFSM(name string, typ string, begin int, end int) string {
 		cfsm += fmt.Sprintf("%s %d ? STOP %s\n", q0, i, qTerm)
 		for j := begin; j < end; j++ {
 			if i != j {
-				cfsm += fmt.Sprintf("%s %d ! STOP %s\n", qTerm, j, qTerm)
+				//cfsm += fmt.Sprintf("%s %d ! STOP %s\n", qTerm, j, qTerm)
+				cfsm += fmt.Sprintf("%s %d ! %s %s\n", qTerm, j, encodeSymbols(typ), qTerm)
 			}
 		}
 	}
 
-	return fmt.Sprintf("-- channel %s\n.outputs\n.state graph\n%s.marking %s\n.end\n\n", name, cfsm, q0)
+	return fmt.Sprintf("-- channel %s\n.outputs\n.state graph\n%s.marking %s\n.end\n\n", role.Name(), cfsm, q0)
 }
 
 // nodeToCFSM creates CFSM states from sesstype.Node. q0 is already written.
 func nodeToCFSM(root sesstype.Node, role sesstype.Role, q0 string, initial bool) string {
 	switch node := root.(type) {
 	case *sesstype.SendNode:
-		toCFSM, ok := cfsmByName[node.To().Name()]
+		toCFSM, ok := cfsmByName[node.To()]
 		if !ok {
 			panic(fmt.Sprintf("Sending to unknown channel: %s", node.To().Name()))
 		}
 
 		sendType := encodeSymbols(node.To().Type().String())
-		qSend := encodeSymbols(genNewState(role.Name()))
+		qSend := encodeSymbols(genNewState(role))
 		cfsm := fmt.Sprintf("%s %d ! %s ", q0, toCFSM, sendType)
 		if !initial {
 			cfsm = fmt.Sprintf("%s\n%s", q0, cfsm)
@@ -101,21 +102,21 @@ func nodeToCFSM(root sesstype.Node, role sesstype.Role, q0 string, initial bool)
 			childrenCfsm += childCfsm
 		}
 		if childrenCfsm == "" {
-			if _, ok := finalState[role.Name()]; !ok {
-				finalState[role.Name()] = qSend
+			if _, ok := finalState[role]; !ok {
+				finalState[role] = qSend
 			}
-			return fmt.Sprintf("%s%s\n", cfsm, finalState[role.Name()])
+			return fmt.Sprintf("%s%s\n", cfsm, finalState[role])
 		}
 		return fmt.Sprintf("%s %s", cfsm, childrenCfsm)
 
 	case *sesstype.RecvNode:
-		fromCFSM, ok := cfsmByName[node.From().Name()]
+		fromCFSM, ok := cfsmByName[node.From()]
 		if !ok {
 			panic(fmt.Sprintf("Receiving from unknown channel: %s", node.From().Name()))
 		}
 
 		recvType := encodeSymbols(node.From().Type().String())
-		qRecv := encodeSymbols(genNewState(role.Name()))
+		qRecv := encodeSymbols(genNewState(role))
 		cfsm := fmt.Sprintf("%s %d ? %s ", q0, fromCFSM, recvType)
 		if !initial {
 			cfsm = fmt.Sprintf("%s\n%s", q0, cfsm)
@@ -127,20 +128,20 @@ func nodeToCFSM(root sesstype.Node, role sesstype.Role, q0 string, initial bool)
 			childrenCfsm += childCfsm
 		}
 		if childrenCfsm == "" {
-			if _, ok := finalState[role.Name()]; !ok {
-				finalState[role.Name()] = qRecv
+			if _, ok := finalState[role]; !ok {
+				finalState[role] = qRecv
 			}
-			return fmt.Sprintf("%s%s\n", cfsm, finalState[role.Name()])
+			return fmt.Sprintf("%s%s\n", cfsm, finalState[role])
 		}
 		return fmt.Sprintf("%s %s", cfsm, childrenCfsm)
 
 	case *sesstype.EndNode:
-		endCFSM, ok := cfsmByName[node.Chan().Name()]
+		endCFSM, ok := cfsmByName[node.Chan()]
 		if !ok {
 			panic(fmt.Sprintf("Closing unknown channel: %s", node.Chan().Name()))
 		}
 
-		qEnd := encodeSymbols(genNewState(role.Name()))
+		qEnd := encodeSymbols(genNewState(role))
 		cfsm := fmt.Sprintf("%s %d ! STOP ", q0, endCFSM)
 		if !initial {
 			cfsm = fmt.Sprintf("%s\n%s", q0, cfsm)
@@ -152,10 +153,10 @@ func nodeToCFSM(root sesstype.Node, role sesstype.Role, q0 string, initial bool)
 			childrenCfsm += childCfsm
 		}
 		if childrenCfsm == "" {
-			if _, ok := finalState[role.Name()]; !ok {
-				finalState[role.Name()] = qEnd
+			if _, ok := finalState[role]; !ok {
+				finalState[role] = qEnd
 			}
-			return fmt.Sprintf("%s%s\n", cfsm, finalState[role.Name()])
+			return fmt.Sprintf("%s%s\n", cfsm, finalState[role])
 		}
 		return fmt.Sprintf("%s %s", cfsm, childrenCfsm)
 
@@ -171,6 +172,7 @@ func nodeToCFSM(root sesstype.Node, role sesstype.Role, q0 string, initial bool)
 	case *sesstype.LabelNode:
 		labelJumpState[node.Name()] = q0
 		cfsm, childInit := "", initial
+		//cfsm += fmt.Sprintf("Label{label:%s,state:%s}\n", node.Name(), q0)
 		for _, child := range node.Children() {
 			childCfsm := nodeToCFSM(child, role, q0, childInit)
 			childInit = (childCfsm != "" || initial)
@@ -181,11 +183,15 @@ func nodeToCFSM(root sesstype.Node, role sesstype.Role, q0 string, initial bool)
 	case *sesstype.GotoNode:
 		qJumpto := labelJumpState[node.Name()]
 		cfsm, childInit := "", initial
+		//cfsm += fmt.Sprintf("Goto{label:%s,state:%s}\n", node.Name(), qJumpto)
 		for _, child := range node.Children() {
 			// qJumpto written, so initial again
 			childCfsm := nodeToCFSM(child, role, qJumpto, childInit)
 			childInit = (childCfsm != "" || initial)
 			cfsm += childCfsm
+		}
+		if cfsm == "" && !initial {
+			return fmt.Sprintf("%s\n", qJumpto)
 		}
 		return cfsm
 
@@ -195,7 +201,7 @@ func nodeToCFSM(root sesstype.Node, role sesstype.Role, q0 string, initial bool)
 }
 
 func genCFSM(role sesstype.Role, root sesstype.Node) string {
-	q0 := encodeSymbols(genNewState(role.Name()))
+	q0 := encodeSymbols(genNewState(role))
 	cfsmBody := nodeToCFSM(root, role, q0, true)
 	if cfsmBody == "" {
 		return ""
@@ -205,11 +211,11 @@ func genCFSM(role sesstype.Role, root sesstype.Node) string {
 
 func PrintCFSMSummary() {
 	fmt.Printf("Total of %d CFSMs (%d are channels)\n", cfsmCount, chanCount)
-	for name, index := range cfsmByName {
+	for role, index := range cfsmByName {
 		if index < chanCount {
-			fmt.Printf("\t%d\t= %s (channel)\n", index, name)
+			fmt.Printf("\t%d\t= %s (channel)\n", index, role.Name())
 		} else {
-			fmt.Printf("\t%d\t= %s\n", index, name)
+			fmt.Printf("\t%d\t= %s\n", index, role.Name())
 		}
 	}
 }
@@ -218,7 +224,7 @@ func PrintCFSMSummary() {
 // CFSMs for the channels.
 func getCFSMs(s *sesstype.Session) string {
 	for _, c := range s.Chans {
-		cfsmByName[c.Name()] = cfsmCount // For role CFSMs
+		cfsmByName[c] = cfsmCount // For role CFSMs
 		chanByIndex = append(chanByIndex, c)
 		cfsmCount++
 	}
@@ -229,8 +235,8 @@ func getCFSMs(s *sesstype.Session) string {
 		machine := genCFSM(role, root)
 		fmt.Fprintf(os.Stderr, "Generate %s CFSM\n", role.Name())
 		if machine != "" {
-			cfsmByName[role.Name()] = cfsmCount
-			roleCFSMs += fmt.Sprintf("-- %d\n", cfsmByName[role.Name()])
+			cfsmByName[role] = cfsmCount
+			roleCFSMs += fmt.Sprintf("-- %d\n", cfsmByName[role])
 			roleCFSMs += machine
 			cfsmCount++
 		} else {
@@ -240,8 +246,8 @@ func getCFSMs(s *sesstype.Session) string {
 
 	chanCFSMs := ""
 	for _, ch := range chanByIndex {
-		chanCFSMs += fmt.Sprintf("-- %d\n", cfsmByName[ch.Name()])
-		chanCFSMs += genChanCFSM(ch.Name(), ch.Type().String(), chanCount, chanCount+cfsmCount-chanCount)
+		chanCFSMs += fmt.Sprintf("-- %d\n", cfsmByName[ch])
+		chanCFSMs += genChanCFSM(ch, ch.Type().String(), chanCount, chanCount+cfsmCount-chanCount)
 	}
 
 	return chanCFSMs + roleCFSMs
