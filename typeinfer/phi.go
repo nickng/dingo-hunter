1c001348f3de4477efa748175eb8353277afccd0
Nicholas Ng <nickng@projectfate.org>
2016-06-30 12:15:02 +0100

    Phi should also try to copy special instance types

--- typeinfer/phi.go
+++ typeinfer/phi.go
@@ -70,11 +70,23 @@ func phiSelectEdge(instr *ssa.Phi, infer *TypeInfer, f *Function, b *Block, l *L
 					infer.Logger.Fatalf("phi: create instance Edge[%d]=%s: %s", i, instr.Edges[i].String()+instr.Edges[i].Name(), ErrUnknownValue)
 				}
 				f.locals[instr], edge = &ConstInstance{c}, pred.Index
-				infer.Logger.Printf(f.Sprintf(PhiSymbol+"%s = %s, selected const from block %d", f.locals[instr], instr.String(), edge))
+				infer.Logger.Printf(f.Sprintf(PhiSymbol+"%s/%s = %s, selected const from block %d", instr.Name(), f.locals[instr], instr.String(), edge))
+				if a, ok := f.arrays[e]; ok {
+					f.arrays[f.locals[instr]] = a
+				}
+				if s, ok := f.structs[e]; ok {
+					f.structs[f.locals[instr]] = s
+				}
 				return edge
 			}
 			f.locals[instr], edge = e, pred.Index
-			infer.Logger.Printf(f.Sprintf(PhiSymbol+"%s = %s, selected from block %d", f.locals[instr], instr.String(), edge))
+			infer.Logger.Printf(f.Sprintf(PhiSymbol+"%s/%s = %s, selected from block %d", instr.Name(), f.locals[instr], instr.String(), edge))
+			if a, ok := f.arrays[e]; ok {
+				f.arrays[f.locals[instr]] = a
+			}
+			if s, ok := f.structs[e]; ok {
+				f.structs[f.locals[instr]] = s
+			}
 			return edge
 		}
 	}
