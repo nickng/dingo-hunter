227596c9d759530110f97163dfd23c4fe09c751d
Nicholas Ng <nickng@projectfate.org>
2016-07-02 17:02:29 +0100

    Handle functions in Phi

--- typeinfer/phi.go
+++ typeinfer/phi.go
@@ -7,37 +7,36 @@ import (
 )
 
 // phiSelectEdge selects edge based on predecessor block and returns the edge index.
-func phiSelectEdge(instr *ssa.Phi, infer *TypeInfer, f *Function, b *Block, l *Loop) int {
-	edge := 0
+func phiSelectEdge(instr *ssa.Phi, infer *TypeInfer, f *Function, b *Block, l *Loop) (edge int) {
 	for i, pred := range instr.Block().Preds {
 		if pred.Index == b.Pred {
 			e, ok := f.locals[instr.Edges[i]]
 			if !ok {
-				c, ok := instr.Edges[i].(*ssa.Const)
-				if !ok {
+				switch t := instr.Edges[i].(type) {
+				case *ssa.Const:
+					f.locals[instr.Edges[i]] = &ConstInstance{t}
+					e, edge = f.locals[instr.Edges[i]], pred.Index
+					infer.Logger.Printf(f.Sprintf(PhiSymbol+"%s/%s = %s, selected const from block %d", instr.Name(), e, instr.String(), edge))
+				case *ssa.Function:
+					f.locals[instr.Edges[i]] = &Instance{instr.Edges[i], f.InstanceID(), l.Index}
+					e, edge = f.locals[instr.Edges[i]], pred.Index
+					infer.Logger.Printf(f.Sprintf(PhiSymbol+"%s/%s = %s, selected function from block %d", instr.Name(), e, instr.String(), edge))
+				default:
 					infer.Logger.Fatalf("phi: create instance Edge[%d]=%#v: %s", i, instr.Edges[i], ErrUnknownValue)
+					return
 				}
-				f.locals[instr], edge = &ConstInstance{c}, pred.Index
-				infer.Logger.Printf(f.Sprintf(PhiSymbol+"%s/%s = %s, selected const from block %d", instr.Name(), f.locals[instr], instr.String(), edge))
-				if a, ok := f.arrays[e]; ok {
-					f.arrays[f.locals[instr]] = a
-				}
-				if s, ok := f.structs[e]; ok {
-					f.structs[f.locals[instr]] = s
-				}
-				return edge
 			}
 			f.locals[instr], edge = e, pred.Index
-			infer.Logger.Printf(f.Sprintf(PhiSymbol+"%s/%s = %s, selected from block %d", instr.Name(), f.locals[instr], instr.String(), edge))
+			infer.Logger.Printf(f.Sprintf(PhiSymbol+"%s/%s = %s, selected from block %d", instr.Name(), e, instr.String(), edge))
 			if a, ok := f.arrays[e]; ok {
 				f.arrays[f.locals[instr]] = a
 			}
 			if s, ok := f.structs[e]; ok {
 				f.structs[f.locals[instr]] = s
 			}
-			return edge
+			return
 		}
 	}
 	infer.Logger.Fatalf("phi: %d->%d: %s", b.Pred, instr.Block().Index, ErrPhiUnknownEdge)
-	return edge
+	return
 }
